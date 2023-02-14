58af370ed72e1523f99a6750419d06a425fed62f
Max Chui <max@maxchui.dev>
2023-02-14 22:37:42 +0800

    feat: Additional lossless image files mapping and added MP4 folder

--- Constants.py
+++ Constants.py
@@ -1,9 +1,39 @@
-CAMERA_FOLDER_NAME = ['RAW', 'JPG', 'HIF']
+# Folders to be created in the root folder
+CAMERA_FOLDER_NAME = ['RAW', 'JPG', 'HIF', 'MP4']
 
+# File extensions to be considered
 CAMERA_FILE_TO_FOLDER_MAPPING = {
-    "ARW": "RAW",
-    "RAW": "RAW",
+    # RAW IMAGES
+    "ARW": "RAW",  # SONY
+    "RAW": "RAW",  # SONY
+    "CR2": "RAW",  # CANON
+    "CR3": "RAW",  # CANON
+    "NEF": "RAW",  # NIKON
+    "ORF": "RAW",  # OLYMPUS
+    "RW2": "RAW",  # PANASONIC
+    "RAF": "RAW",  # FUJI
+    "SR2": "RAW",  # SONY
+    "SRF": "RAW",  # SONY
+    "DNG": "RAW",  # ADOBE
+    "PEF": "RAW",  # PENTAX
+    "MRW": "RAW",  # MINOLTA
+    "KDC": "RAW",  # KODAK
+    "3FR": "RAW",  # HASSELBLAD
+    "FFF": "RAW",  # IMAGEMATE
+    "MEF": "RAW",  # MAMIYA
+    "ERF": "RAW",  # EPSON
+    "IIQ": "RAW",  # PHASE ONE
+    "MOS": "RAW",  # IMAGEMATE
+    "NRW": "RAW",  # NIKON
+    "PTX": "RAW",  # PANASONIC
+    "R3D": "RAW",  # RED
+    "X3F": "RAW",  # SIGMA
+
+    # COMPRESSED IMAGES
     "JPG": "JPG",
     "JPEG": "JPG",
     "HIF": "HIF",
+
+    # VIDEOS
+    'MP4': 'MP4',
 }
