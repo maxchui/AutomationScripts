3c206aa09cc8637e4bfccfea8d625245685bbd24
Max Chui <test@test.com>
2023-02-14 21:53:19 +0800

    fix: Incorrect logger config

--- CameraFilesSort.py
+++ CameraFilesSort.py
@@ -1,4 +1,4 @@
-import logging
+import logging.config
 import os
 import shutil
 import tkinter as tk
@@ -20,24 +20,28 @@ def main():
         base_folder_path = filedialog.askdirectory(
             title='Please select the folder where you have placed the image files')
 
-        if not os.path.exists(base_folder_path+"/logs"):
+        if not os.path.exists(base_folder_path + "/logs"):
             logging.debug(f'Log folder does not exist, creating log directory')
             os.mkdir(f'{base_folder_path}/logs')
     except Exception as e:
         logging.exception("message")
-    finally:
-        # Initialise logging parameters
-        LOG_FORMAT = '%(asctime)s %(levelname)s: %(message)s'
-        logging.basicConfig(level=logging.DEBUG,
-                            filename=f'{base_folder_path}/logs/CameraFilesSort_{datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}.log',
-                            filemode='w', format=LOG_FORMAT)
-        logging.debug(f'Started log file in path = {base_folder_path}')
+
+    # Initialise logging parameters
+    LOG_FORMAT = '%(asctime)s %(levelname)s: %(message)s'
+    logging.basicConfig(level=logging.DEBUG,
+                        handlers=[logging.FileHandler(
+                            f'{base_folder_path}/logs/CameraFilesSort_{datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}.log'),
+                                  logging.StreamHandler()],
+                        format=LOG_FORMAT, force=True)
+    logger = logging.getLogger(__name__)
+    print(logger)
+    logger.debug(f'Started log file in path = {base_folder_path}')
 
     file_entries = os.listdir(base_folder_path)
 
     # Terminate the program if the folder has nothing in it (the user is supposed to move the files into the folder first)
     if len(file_entries) == 0:
-        logging.error("The folder is empty. Terminating...")
+        logger.error("The folder is empty. Terminating...")
         quit()
 
     # Create the sub-folders to store the files with the same file extension
@@ -45,9 +49,9 @@ def main():
         if i not in file_entries:
             try:
                 os.mkdir(base_folder_path + '/' + i)
-                logging.debug(f'Folder {i} does not exist, creating directory {i}')
+                logger.debug(f'Folder {i} does not exist, creating directory {i}')
             except:
-                logging.exception("message")
+                logger.exception("message")
 
     # Time to move dem files :D
     for entry in os.listdir(base_folder_path):
@@ -62,10 +66,10 @@ def main():
                     source_address = os.path.join(base_folder_path, entry)
                     destination_address = os.path.join(base_folder_path, CAMERA_FILE_TO_FOLDER_MAPPING[file_extension])
                     shutil.move(source_address, destination_address)
-                    logging.debug(f'Moving file {entry} from {source_address} to {destination_address}')
+                    logger.debug(f'Moving file {entry} from {source_address} to {destination_address}')
                 except:
-                    logging.exception("message")
+                    logger.exception("message")
             else:
-                logging.error(f'Extension {file_extension} does not exist in the mapping table (entry={entry})')
+                logger.error(f'Extension {file_extension} does not exist in the mapping table (entry={entry})')
 
-    logging.info("Terminating program")
+    logger.info("Terminating program")
